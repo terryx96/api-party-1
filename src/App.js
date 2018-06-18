48ee8dbfde862f209aa975c195ec659dbe55f776
Dave Strus <dave@getfretless.com>
2018-06-18 14:37:48 -0400

    Style the app

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>API Party</h1>
+        <div className="App-header">
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
+        </div>
 
-        <ul>
+        <ul className="navLinks">
           <li>
             <NavLink to="/github">GitHub</NavLink>
           </li>
